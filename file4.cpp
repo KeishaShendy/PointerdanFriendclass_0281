abcc8506c644790682cd127fe3621ed332a2caa7
keishashendy <shendyarrafli7@gmail.com>
2025-05-06 11:25:22 +0700

    menambahkan fungsi friend bernama setNama

--- file4.cpp
+++ file4.cpp
@@ -7,3 +7,19 @@ class mahasiswa
 private:
     string nama;
 
+    public:
+    friend void setNama(mahasiswa &a, string b);
+};
+
+void setNama(mahasiswa &a, string b)
+{
+    a.nama = b;
+    cout << a.nama;
+}
+
+int main()
+{
+    mahasiswa joko;
+    setNama(joko, "Kairi Kumar");
+    return 0;
+}
